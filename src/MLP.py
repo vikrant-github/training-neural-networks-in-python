6f0e2480c17fd980d9b26659ac99928dcb0e3a0c
vikrant-github <vikrantmails@aol.com>
2023-06-14 04:11:50 +0000

    06 Multilayer Perceptron Challenge

--- src/MLP.py
+++ src/MLP.py
@@ -8,7 +8,7 @@ class Perceptron:
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        self.weights = (np.random.rand(inputs+1) * 2) - 1 
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
         self.bias = bias
 
     def run(self, x):
@@ -43,9 +43,9 @@ class MultiLayerPerceptron:
         for i in range(len(self.layers)):
             self.values.append([])
             self.network.append([])
-            self.values[i] = [0.0 for j in range(self.layers[i])]
+            self.values[i] = [0.0 for j in range(self.layers[i])] # creates a list of zeros with a length equal to the number of neurons in the ith layer of a neural network
             if i > 0:      #network[0] is the input layer, so it has no neurons
-                for j in range(self.layers[i]): 
+                for j in range(self.layers[i]):
                     self.network[i].append(Perceptron(inputs = self.layers[i-1], bias = self.bias))
         
         self.network = np.array([np.array(x) for x in self.network],dtype=object)
@@ -54,9 +54,11 @@ class MultiLayerPerceptron:
 # Challenge: Finish the set_weights() and run() methods:
 
     def set_weights(self, w_init):
-        # Write all the weights into the neural network.
-        # w_init is a list of floats. Organize it as you'd like. 
-        pass       
+        """Set the weights.
+           w_init is a list of lists with the weights for all but the input layer."""
+        for i in range(len(w_init)):
+            for j in range(len(w_init[i])):
+                self.network[i+1][j].set_weights(w_init[i][j])
 
     def print_weights(self):
         print()
@@ -66,7 +68,20 @@ class MultiLayerPerceptron:
         print()
 
     def run(self, x):
-        # Run an input forward through the neural network.
-        # x is a python list with the input values.
+        """Feed a sample x into the MultiLayer Perceptron."""
+        x = np.array(x, dtype=object)
+        self.values[0] = x
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                self.values[i][j] = self.network[i][j].run(self.values[i-1])
         return self.values[-1]
-        
+    
+# test code
+mlp = MultiLayerPerceptron(layers=[2,2,1])  #mlp
+mlp.set_weights([[[-10,-10,15],[15,15,-10]],[[10,10,-15]]])
+mlp.print_weights()
+print("MLP:")
+print ("0 0 = {0:.10f}".format(mlp.run([0,0])[0]))
+print ("0 1 = {0:.10f}".format(mlp.run([0,1])[0]))
+print ("1 0 = {0:.10f}".format(mlp.run([1,0])[0]))
+print ("1 1 = {0:.10f}".format(mlp.run([1,1])[0]))
